d4e39c6cfb04323da157303e24ea62bf8c8d0b21
Gokul <gokulchandan24@gmail.com>
2024-07-02 17:50:39 +0530

    created a new app

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import Home from "./pages/home";
+import Category from "./pages/category";
+import SearchPage from "./pages/search";
+import Favorites from "./pages/favorites";
+import Single_Gif from "./pages/single-gif";
+import GifProvider from "./context/gif-context";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:type/:slug",
+        element: <Single_Gif />,
+      },
+      {
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+        element: <SearchPage />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <>
+      <GifProvider>
+        <RouterProvider router={router} />
+      </GifProvider>
+    </>
+  );
+};
+
+export default App;
